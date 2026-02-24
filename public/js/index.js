9f0643a2e414a00e85cd0609dfe1edbef5429dba
Folly Teko <follyprosper01@gmail.com>
2026-02-24 18:53:55 -0500

    updated packge.json and added index.js

---
+++ public/js/index.js
@@ -0,0 +1,37 @@
+"use strict"
+const elements = {
+    quote: document.getElementById("quote"),
+    author: document.getElementById("author"),
+};
+
+const quotes = [
+    {
+        quote: "All hands! Abandon ship!",
+        author: "Captain Picard",
+    },
+
+    {
+        quote: "Doh!",
+        author: "Homer Simpson",
+    },
+
+    {
+        quote: "The development of full artificial intelligence could spell the end of the human race.",
+        author: "Stephen Hawking",
+    }
+]
+
+function loopThroughQuotes() {
+    let quoteIndex = 0;
+    setInterval(() => {
+        if (quoteIndex < quotes.length) {
+            elements.quote.textContent = quotes[quoteIndex].quote;
+            elements.author.textContent = quotes[quoteIndex].author;
+            quoteIndex++;
+        } else {
+            quoteIndex = 0;
+        }
+    }, 3000);
+}
+
+setTimeout(loopThroughQuotes, 3000);
