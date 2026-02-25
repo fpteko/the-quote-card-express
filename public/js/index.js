3be467ebaac3d04b071c98a2fddd642359e494ff
Folly Teko <follyprosper01@gmail.com>
2026-02-24 19:51:58 -0500

    Added code to index.js to use API key

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,24 @@ const elements = {
     author: document.getElementById("author"),
 };
 
+async function getRandomImage() {
+    const client_id = "Acess_Key_Here";
+    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    try {
+        const response = await fetch(endpoint);
+        const returnedData = await response.json();
+        const receivedPhotoUrl = returnedData.urls.regular;
+
+        const imgDiv = document.querySelector(".background-img");
+        imgDiv.style.backgroundImage = `url(${receivedPhotoUrl})`;
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+getRandomImage();
+
+/*
 const quotes = [
     {
         quote: "All hands! Abandon ship!",
@@ -34,4 +52,4 @@ function loopThroughQuotes() {
     }, 3000);
 }
 
-setTimeout(loopThroughQuotes, 3000);
+setTimeout(loopThroughQuotes, 3000); */
