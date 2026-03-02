2052cafa45212d1defa5d686e643db7d6faaaf3d
Folly Teko <follyprosper01@gmail.com>
2026-03-01 19:26:48 -0500

    updtaed server.js and index.js to make a callout but keep API key secret

--- server.js
+++ server.js
@@ -2,11 +2,37 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+
 app.use(express.static("./public"));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+require("dotenv").config();
+const cors = require("cors");
 
+async function getRandomImage() {
+    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${process.env.CLIENT_ID}`;
+    try {
+        const response = await fetch(endpoint);
+        const returnedData = await response.json();
+        const receivedPhotoUrl = returnedData.urls.regular;
+
+        return receivedPhotoUrl;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const corsOptions = {
+    origin: `http://localhost:${port}`
+}
+app.use(cors());
+app.use("/api/v1/getRandomImage", async (request, response) => {
+    response.status(200).json({
+        status: 200,
+        data: await getRandomImage(),
+    });
+});
 app.listen(port, () => {
     console.log(`server is running http://localhost:${port}`);
     console.log("Press Ctrl+C to end this process");
